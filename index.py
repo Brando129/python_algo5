be772af0e43b9e8ed20c269e91ac4a56f2ac55ed
Brandon Colangelo <brandoncancode@gmail.com>
2023-05-01 09:27:27 -0400

    Update index.py
    Added some documentation

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-# Create a function that takes a users array and set value and pushes the greater
-# values into a new array, as well as keeps count of how many items in
-# the array are greater than the set value
+# Create a function that takes a users list and set value and pushes the greater
+# list values into a new list, as well as keeping count of how many items in
+# the users list are greater than the set value
 
 def greater_than(list, value):
     new_list = []
